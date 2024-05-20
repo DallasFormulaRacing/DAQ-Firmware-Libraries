65a50196f38ca74195431088a1a5ef6686d10a54
NicoOhR <nimoohayonr@gmail.com>
2024-05-19 22:44:13 -0500

    decoupling relay and logger

--- Application/DataLogger/DataLogger.hpp
+++ Application/DataLogger/DataLogger.hpp
@@ -52,7 +52,6 @@ public:
 	DataLogger(std::shared_ptr<IFileSystem> file_system,
 			   std::shared_ptr<platform::IGpio> user_input,
 			   CircularQueue<DataPayload>& queue,
-			   Can_Relay relay,
 			   uint8_t& storage_connected_observer,
 			   bool& logging_enabled_sharer);
 
@@ -111,7 +110,6 @@ private:
 	std::shared_ptr<IFileSystem> file_system_;
 	std::shared_ptr<platform::IGpio> user_input_;
 	CircularQueue<DataPayload>& queue_;
-	Can_Relay can_relay_;
 	uint8_t& storage_connected_observer_;
 	bool& logging_enabled_;
 
