fa90abce88bb568652601a2bf19018172d3efa60
William Kim <73252368+wxkim@users.noreply.github.com>
2024-08-22 00:19:25 -0500

    adc interface created

---
+++ Platform/Interfaces/iadc.hpp
@@ -0,0 +1,34 @@
+/*
+ * STM32 F4 ANALOG TO DIGITAL CONVERTER ABSTRACT INTERFACE
+ * Author:   William Kim
+ *
+ * Email:    wkim@utdallas.edu
+ *
+ * (c) 2024 Dallas Formula Racing - Embedded Firmware Team
+ * Formula SAE International Collegiate Chapter
+ * GPL-3.0 License
+ */
+
+#ifndef IADC_H
+#define IADC_H
+
+namespace platform
+{
+
+    class IADC
+    {
+    public:
+        virtual ~IADC() = default;
+
+        virtual void Initialize_ADC() = 0;
+
+        virtual void start() = 0;
+        virtual void stop() = 0;
+
+        virtual double read() = 0;
+
+        virtual /* float? */ double Get_ADC_Result() = 0; // retrieve the converted result
+    };
+}
+
+#endif
