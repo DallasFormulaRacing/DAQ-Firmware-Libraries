589bdd74c2b9642a90116570c6f69fffe09b3ea4
Aditya Janan <69063339+TheAditya123@users.noreply.github.com>
2024-09-28 19:42:47 -0500

    Update imu.cpp

--- Sensor/IMU/imu.cpp
+++ Sensor/IMU/imu.cpp
@@ -5,7 +5,7 @@ namespace sensor {
 
 LSM6DSOX_IMU::LSM6DSOX_IMU(I2C_HandleTypeDef &hi2c)
     : gyroscope_(hi2c), accelerometer_(hi2c) {
-} //LSM6DSOX_IMU inherits from both Accelerometer and Gyroscope 
+}
 
 void LSM6DSOX_IMU::Initialize() {
     gyroscope_.SetODR(LSM6DSOX_Gyroscope::ODR::ODR104);
@@ -40,6 +40,7 @@ float* LSM6DSOX_IMU::RadiansPerSecond() {
 void LSM6DSOX_IMU::updateSensorData() {
     accelerometer_.ComputeAcceleration();
     gyroscope_.GetAngularVelocity();  // Use the public wrapper method to access private method
+
     for (int i = 0; i < 3; ++i) {
         acceleration_[i] = accelerometer_.GetAcceleration()[i];
         angular_velocity_[i] = gyroscope_.DegreesPerSecond()[i] * gyroscope_.GetDegreesToRadians();  // Use the public wrapper method
